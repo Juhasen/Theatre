5e112b9f216f18dd37aaf5ecbac8ac67ff3205ce
Juhasen <juszczyk-krystian@wp.pl>
2025-04-04 20:11:26 +0200

    Update README.md

---
+++ src/main/java/pl/juhas/rezerwacje_teatr/RezerwacjeTeatrApplication.java
@@ -0,0 +1,13 @@
+package pl.juhas.rezerwacje_teatr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezerwacjeTeatrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RezerwacjeTeatrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/juhas/rezerwacje_teatr/RezerwacjeTeatrApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.juhas.rezerwacje_teatr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RezerwacjeTeatrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
