045527a8af74018dcbbebc47c4fc9f6cc4f9663f
Juhasen <juszczyk-krystian@wp.pl>
2025-04-13 21:09:06 +0200

    Added logger to tests

--- src/test/java/pl/juhas/theater/TheaterApplicationTests.java
+++ src/test/java/pl/juhas/theater/TheaterApplicationTests.java
@@ -1,10 +1,13 @@
 package pl.juhas.theater;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-
+@Slf4j
 @SpringBootTest
+@Transactional
 class TheaterApplicationTests {
 
     @Test
