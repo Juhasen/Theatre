7ee17b76b7507d7eaa5b786efc4eea2ea94a6082
Juhasen <juszczyk-krystian@wp.pl>
2025-04-13 21:17:22 +0200

    Created repositories

---
+++ src/main/java/pl/juhas/theater/repository/PerformanceRepository.java
@@ -0,0 +1,8 @@
+﻿package pl.juhas.theater.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.juhas.theater.model.Performance;
+
+public interface PerformanceRepository extends JpaRepository<Performance, Long> {
+
+}
---
+++ src/main/java/pl/juhas/theater/repository/UserRepository.java
@@ -0,0 +1,8 @@
+﻿package pl.juhas.theater.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.juhas.theater.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
