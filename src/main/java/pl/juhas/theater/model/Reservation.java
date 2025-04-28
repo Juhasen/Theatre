96b3aeb49bc752c6bcd9e8dddca0ecec4d4ff3a4
Juhasen <juszczyk-krystian@wp.pl>
2025-04-28 22:16:39 +0200

    Created SeatRepo

--- src/main/java/pl/juhas/theater/model/Reservation.java
+++ src/main/java/pl/juhas/theater/model/Reservation.java
@@ -5,7 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.With;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
@@ -35,5 +37,8 @@ public class Reservation {
 
     @OneToMany
     private List<Seat> seat;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    private LocalDateTime createdAt;
 
 }
